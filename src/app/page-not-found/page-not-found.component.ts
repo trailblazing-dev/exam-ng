35434dd1cd497a2cc44f3d6bbde0cb668c66d83d
trailblazing-dev <info@trailblazing.dev>
2021-08-02 13:31:04 -0700

    add Page-Not-Found component

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-not-found.component.sass']
 })
 export class PageNotFoundComponent implements OnInit {
+  oopsClass: string = 'oops-'
+  n404Class: string = 'n404-'
+  msgClass: string = 'msg-'
+  backClass: string = 'back-'
+
+  @Input() template: string = 'default'
 
   constructor() { }
 
   ngOnInit(): void {
+    this.oopsClass += this.template
+    this.n404Class += this.template
+    this.msgClass += this.template
+    this.backClass += this.template
   }
 
 }
